ed07c192a1b3453e85962dae41a93cf9b6c78b5f
Juliana Correa <jucorrea.mail@gmail.com>
2016-03-27 01:17:11 -0300

    adding flapper news based on thikster tutorial

---
+++ angular/posts-thinkster/app.js
@@ -0,0 +1,29 @@
+angular.module('flapperNews', [])
+
+   .controller('MainCtrl', ['$scope', function($scope){
+
+      var self = this;
+
+      self.posts = [                      //mock posts
+         {title: 'post 1', upvotes: 35},
+         {title: 'post 2', upvotes: 13},
+         {title: 'post 3', upvotes: 17},
+         {title: 'post 4', upvotes: 8},
+         {title: 'post 5', upvotes: 14}
+      ];
+
+      self.add = function(){
+         if(!self.title || self.title === '') { return; }
+         self.posts.push({'title': self.title, 'link': self.link, 'upvotes' : 0});
+         self.title = '';
+         self.link = '';
+      };
+
+      self.incrementUpvotes = function(post) {
+         post.upvotes += 1;
+      };
+
+      self.decrementUpvotes = function(post) {
+         post.upvotes -= 1;
+      };
+   }]);
--- angular/todoapp-treehouse/scripts/app.js
+++ angular/todoapp-treehouse/scripts/app.js
@@ -3,57 +3,57 @@ angular.module("todoListApp", [])
 // MAIN CONTROLLER
 
 // dataService is the name of a service being injected (DI)
-.controller('mainCtrl', function($scope, dataService) { 
-  
+.controller('mainCtrl', function($scope, dataService) {
+
   $scope.helloService = dataService.hello;
 
   $scope.helloWorld = function(index) {
     console.log("Element " + index + " clicked. Setting focus to input not working properly yet");
   }
-  
-  dataService.getTodos(function(response) { 
+
+  dataService.getTodos(function(response) {
     console.log(response.data);
     $scope.todos = response.data;
   });
-  
+
   $scope.deleteTodo = function(todo, index) {
     dataService.deleteTodo(todo);
     $scope.todos.splice(index, 1) // splice is a standard js method used in todos array
   }
-  
+
   $scope.saveTodos = function() {
     var editedItems = $scope.todos.filter(function(todo) {
       return todo.edited;
-    });    
+    });
     dataService.saveTodos(editedItems);
   }
-  
+
   $scope.addTodo = function() {
     var todo = { name: "New todo", completed: false };
-    $scope.todos.unshift(todo);  // adds to the beginning - push(todo) would add to the end    
+    $scope.todos.unshift(todo);  // adds to the beginning - push(todo) would add to the end
   };
 })
 
 // DATA SERVICE
 
-// $http is a provider being injected to the service 
+// $http is a provider being injected to the service
 //REF https://docs.angularjs.org/api/ng/service/$http
-.service('dataService', function($http) { 
-  
-  this.hello = function(){ 
+.service('dataService', function($http) {
+
+  this.hello = function(){
     console.log ('Hello from the outside');
   };
-  
-  // get and then are chained methods of $http. 
+
+  // get and then are chained methods of $http.
   // getTodos must be called with a callback function as argument which must also have the response as its parameter. This way, the callback deals with the response data only after 'then' method has received it.
-  this.getTodos = function(sucessCallback) {   
-    $http.get('mock/todos.json').then(sucessCallback) 
+  this.getTodos = function(sucessCallback) {
+    $http.get('mock/todos.json').then(sucessCallback)
   };
-  
+
   this.deleteTodo = function(todo) {
     console.log ( todo.name + " has been deleted");
   };
-  
+
   this.saveTodos = function(editedItems) {
     console.log ( editedItems.length + " items have been saved");
   };
