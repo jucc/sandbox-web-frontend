2249f0ba2af6b838946a9fbb32f714e71cef7c56
Juliana Cavalcanti <jucorrea.mail@gmail.com>
2016-05-26 11:14:59 -0300

    adding posts detail page with state, controller and link

--- angular/posts-thinkster/app.js
+++ angular/posts-thinkster/app.js
@@ -4,15 +4,21 @@ angular.module('flapperNews', ['ui.router'])
    .config(['$stateProvider', '$urlRouterProvider',
             function($stateProvider, $urlRouterProvider) {
 
-     $stateProvider
-       .state('home', {
-         url: '/home',
-         templateUrl: '/home.html',
-         controller: 'MainCtrl',
-         controllerAs: 'main'
-       });
-
-     $urlRouterProvider.otherwise('home');
+      $stateProvider
+         .state('home', {
+            url: '/home',
+            templateUrl: '/home.html',
+            controller: 'MainCtrl',
+            controllerAs: 'main'
+         })
+         .state('posts', {
+            url: '/posts/{id}',
+            templateUrl: '/posts.html',
+            controller: 'PostsCtrl',
+            controllerAs: 'detail'
+         })
+      ;
+      $urlRouterProvider.otherwise('home');
    }])
 
    .controller('MainCtrl', ['posts', function(posts){
@@ -22,8 +28,15 @@ angular.module('flapperNews', ['ui.router'])
       self.posts = posts.posts;
 
       self.add = function(){
-         if(!self.title || self.title === '') { return; }
-         self.posts.push({'title': self.title, 'link': self.link, 'upvotes' : 0});
+
+         if(!self.title || self.title === '') {
+            return;
+         }
+         self.posts.push({
+            'title': self.title,
+            'link': self.link,
+            'upvotes' : 0
+         });
          self.title = '';
          self.link = '';
       };
@@ -37,14 +50,48 @@ angular.module('flapperNews', ['ui.router'])
       };
    }])
 
+   .controller('PostsCtrl', ['$stateParams', 'posts', function($stateParams, posts){
+
+      var self = this;
+      // $stateParams retrieves the info in the url such as posts/1/
+      self.post = posts.posts[$stateParams.id];
+   }])
+
    .factory('posts', [function(){
       var obj = {
          posts: [                      //mock posts
-            {title: 'post 1', upvotes: 35},
-            {title: 'post 2', upvotes: 13},
-            {title: 'post 3', upvotes: 17},
-            {title: 'post 4', upvotes: 9},
-            {title: 'post 5', upvotes: 14}
+            {
+               title: 'post 1',
+               upvotes: 35,
+               comments: [
+                  {
+                     author: 'Joe',
+                     body: 'Cool post!',
+                     upvotes: 0
+                  },
+                  {
+                     author: 'Bob',
+                     body: 'Great idea but everything is wrong!',
+                     upvotes: 0
+                  }
+               ]
+            },
+            {
+               title: 'post 2',
+               upvotes: 13
+            },
+            {
+               title: 'post 3',
+               upvotes: 17
+            },
+            {
+               title: 'post 4',
+               upvotes: 9
+            },
+            {
+               title: 'post 5',
+               upvotes: 14
+            }
          ]
       };
       return obj;
